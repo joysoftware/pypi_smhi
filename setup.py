26f6a7bf8a9e750428cbbe7874014948b89e5123
helto4real <tomas.hellstrom@yahoo.se>
2021-05-02 16:26:30 +0000

    1.0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smhi_pkg",
-    version="1.0.14",
+    version="1.0.15",
     author="helto4real",
     author_email="info@joysoftware.org",
     description="Gets the weather forecast data from Swedish " "weather institute",
